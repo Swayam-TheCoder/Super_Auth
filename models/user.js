1b35bfbdc5ca42367e5fecd3795830c705069ae8
Swayam-The Coder <swayamcode1@gmail.com>
2026-05-03 21:25:00 +0530

    -

--- models/user.js
+++ models/user.js
@@ -20,3 +20,5 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 export default mongoose.model("User", userSchema);
+
+// This code defines a Mongoose schema for a User model in a MERN (MongoDB, Express, React, Node.js) application. The schema includes fields for the user's name, email, password, reset token, and reset token expiration date. The email field is set to be unique to prevent duplicate entries. The schema also includes timestamps to automatically track when each user document is created and updated. Finally, the model is exported for use in other parts of the application.

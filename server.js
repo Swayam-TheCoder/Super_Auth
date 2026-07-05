4a04043030769f5103d84924a43c518232fa5d1d
Swayam-The Coder <swayamcode1@gmail.com>
2026-07-05 23:55:42 +0530

    structure in server.js

--- server.js
+++ server.js
@@ -23,3 +23,5 @@ app.use((err, req, res, next) => {
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port " + process.env.PORT);
 });
+
+// Authentication, authorization, scalability, security --> lets make it more poerful and secure backend
